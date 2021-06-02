d757c763a517c95ef9f2ec2951939488885e0628
Wouter <wouter1602@gmail.com>
2021-06-02 12:19:47 +0200

    Added defines.cpp so Windows can compile

---
+++ defines.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file defines.h
+ * @author Wouter van Velzen (19093861@student.hhs.nl)
+ * @brief List with defines used in the main program
+ * @version 0.1
+ * @date 2021-05-24
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+ 
+ #include "defines.h"
