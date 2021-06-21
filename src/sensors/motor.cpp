7c34b74d1710e6d53bba3725b8e301e639c95b0b
JasperVVL <66367196+JasperVVL@users.noreply.github.com>
2021-06-21 12:47:41 +0200

    Update motor.cpp

--- src/sensors/motor.cpp
+++ src/sensors/motor.cpp
@@ -21,7 +21,10 @@ void setupMotor() {
 #ifdef DEBUG
   Serial.println("[INFO] Completed motor setup.");
 #endif  // DEBUG
-}
+}(
+  
+  
+  
 
 /**
  * @brief Converts te force in Newton to PWM values
@@ -30,7 +33,36 @@ void setupMotor() {
  * @param motorForce Struct with the force in per motor in Newton
  */
 void convertForceToPWM(MotorSettings& data, MotorForce& motorForce) {
+    Newton_links = ;
+  Newton_rechts = ;
+  Newton_zijkant = ;
 
+  //Aansturing motor links
+  if(Newton_links > 0){
+    PWM_Motor_links = (2289.3*Newton_links) - 38.70;
+  }if(Newton_links < 0){
+    PWM_Motor_links = (1892.2*Newton_links) - 43.784;
+  }else{
+    PWM_Motor_rechts = 0;
+  }
+
+  //Aansturing motor rechts
+  if(Newton_rechts > 0){
+    PWM_Motor_rechts = (2265.9*Newton_rechts) - 53.011;
+  }if(Newton_rechts < 0){
+    PWM_Motor_rechts = (2041.3*Newton_rechts) - 34.483;
+  }else{
+    PWM_Motor_rechts = 0;
+  }
+
+  //Aansturing motor zijkant
+  if(Newton_zijkant > 0){
+    PWM_Motor_zijkant = (2320.9*Newton_zijkant) - 43.143;
+  }if(Newton_zijkant < 0){
+    PWM_Motor_zijkant = (1990.5*Newton_zijkant) - 23.693;
+  }else{
+    PWM_Motor_zijkant = 0;
+  }
 }
 
 /**
@@ -38,4 +70,12 @@ void convertForceToPWM(MotorSettings& data, MotorForce& motorForce) {
  *
  * @param motorData
  */
-void setMotorSpeed(MotorSettings& motorData) {}
+void setMotorSpeed(MotorSettings& motorData){
+  MOTOR_PWM1 = analogWrite(PWM_motor_rechts);
+  MOTOR_PWM2 = analogWrite(PWM_motor_links);
+  MOTOR_PWM3 = analogWrite(PWM_motor_zijkant);
+}
+  
+
+
+
