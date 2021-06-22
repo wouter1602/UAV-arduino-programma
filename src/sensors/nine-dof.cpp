6a6c25c56d81c58ca4f388cd1b572cea0eee8663
JasperVVL <66367196+JasperVVL@users.noreply.github.com>
2021-06-22 23:08:01 +0200

    toevoegen kalibratie en object

--- src/sensors/nine-dof.cpp
+++ src/sensors/nine-dof.cpp
@@ -13,14 +13,10 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-
 #include "../../defines.h"
 #include "../lib/MPU9250/SparkFunMPU9250-DMP.h"
 #include "../lib/MPU9250/MPU9250_RegisterMap.h"
-
 MPU9250_DMP imu;
-
-
 
 /**
  * @brief Reads all the gyro data from the 9-axis DoF sensor
@@ -37,7 +33,7 @@ void readGyro(GyroData& data) {
  * @param AccelerometerData struct with accelerometer data
  */
 void readAccelerometer(AccelerometerData& data) {
-    float accelZ = imu.calcAccel(imu.az);
+   // float accelZ = imu.calcAccel(imu.az);
 }
 
 /**
@@ -52,54 +48,55 @@ void readCompass(CompassData& data) {
  * @brief Enables 9-axis DoF sensor
  *
  */
-void setupDoF() {
-  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);    //zet alle sensoren aan
-  imu.setGyroFSR(2000); // Set gyro to 2000 dps
-  imu.setAccelFSR(2); // Set accel to +/-2g
-  imu.setLPF(5); // Set LPF corner frequency to 5Hz
-  imu.setSampleRate(10); // Set sample rate to 10Hz
-  imu.setCompassSampleRate(10); // Set mag rate to 10Hz
-  imu.dmpBegin(DMP_FEATURE_GYRO_CAL |   // Enable gyro cal
-              DMP_FEATURE_SEND_CAL_GYRO,// Send cal'd gyro values
-              10);                   // Set DMP rate to 10 Hz
-#ifdef DEBUG
-  Serial.println("[INFO] Completed setup 9-axis DoF sensor");
-#endif  // DEBUG
+ void setupDoF() {
+	pinMode(RELAY_BLOWERS, OUTPUT);
+	imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);    //zet alle sensoren aan
+	imu.setGyroFSR(2000); // Set gyro to 2000 dps
+	imu.setAccelFSR(2); // Set accel to +/-2g
+	imu.setLPF(5); // Set LPF corner frequency to 5Hz
+	imu.setSampleRate(10); // Set sample rate to 10Hz
+	imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+	imu.dmpBegin(DMP_FEATURE_GYRO_CAL |   // Enable gyro cal
+			  DMP_FEATURE_SEND_CAL_GYRO,// Send cal'd gyro values
+			  10);                   // Set DMP rate to 10 Hz
+	#ifdef DEBUG
+	Serial.println("[INFO] Completed setup 9-axis DoF sensor");
+	#endif  // DEBUG
 }
+
+
+/**
+ * @brief Calibrates 9-axis DoF sensor
+ *
+ */
+void Kalibratie(GyroData& data) { 
+    digitalWrite(RELAY_BLOWERS, LOW);
+		 	 
+	for(uint16_t kalibratie_meting = 0; kalibratie_meting <= 20;){
+		if (imu.fifoAvailable() )
+		{
+		// Use dmpUpdateFifo to update the ax, gx, mx, etc. values
+			if (imu.dmpUpdateFifo() == INV_SUCCESS)
+			{
+			//Serial.println(String(omega) + " dps");
+			}
+		}
+		if(data.omega < 0.2){
+			kalibratie_meting += 1;
+		}	 
+	 }
+}
+
+
 
 /**
  * @brief returns Reference to struct with all the data of the Mems unit
  *
  * @param DoFData Reference Struct with Gyro, Accelerometer and compass data.
  */
- 
 void readDoF(DoFData& data) {
     readGyro(data.gyroData);
     readAccelerometer(data.accelerometerData);
     readCompass(data.compassData);
-  
-  /*     float omega = imu.calcGyro(imu.gz);
-
-   if(omega < 0.0){
-      digitalWrite(kalibratie_led, HIGH);
-      meting = 1;
-   }
-      
-   if ( imu.fifoAvailable() )
-    {
-      // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
-      if ( imu.dmpUpdateFifo() == INV_SUCCESS)
-      {
-        //Serial.println(String(omega) + " dps");
-      }
-    }
-
-    if(meting == 1){
-       avg_omega = (avg_omega + omega)/2;   //Neem het gemiddelde van 2 metingen om grote
-                                         //afwijkingen te voorkomen
-      theta = theta + (avg_omega * dt);    //Integratie van omega om theta te verkrijgen
-         
-    }
-	Serial.println(theta);*/
 }
 
