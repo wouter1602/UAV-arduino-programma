4638c3d14b9792162d0036823473bfe014941d70
KZwinkels <51478274+KZwinkels@users.noreply.github.com>
2021-06-22 14:23:52 +0200

    Update circular_track_controller.cpp

--- src/controller/circular_track_controller.cpp
+++ src/controller/circular_track_controller.cpp
@@ -23,4 +23,48 @@
  */
 void circularTrackController(MotorSettings& motorData, MotorForce& motorForce,
                              TimeOfFlightData& timeOfFlightData,
-                             DoFData& degreesOfFreedomData) {}
+                             DoFData& degreesOfFreedomData) {
+    void loop() {
+  S2 = ToF.sensor2;                    #afstand tot muur sensor 2 in mm
+  S3 = ToF.sensor3;                    #afstand tot muur sensor 3 in mm
+  delta_S = S2-S3;                     #verschil afstand S2 en S3 
+  hoekUAV = atan (delta_S/4200);       #hoek van de UAV
+  switch(hoekUAV) {
+      case hoekUAV > 10 :
+         motorForce.motorForce1 = -1.5
+         motorForce.motorForce2 = 1.0
+         break;
+      case hoekUAV > 20 :
+         motorForce.motorForce1 = -3.0
+         motorForce.motorForce2 = 2.0
+         break;
+      case hoekUAV > 30 :
+         motorForce.motorForce1 = -4.5
+         motorForce.motorForce2 = 3.5
+         break;
+      case hoekUAV > 40 :
+         motorForce.motorForce1 = -6.0
+         motorForce.motorForce2 = 5.0
+         break;
+      case hoekUAV < 10 :
+         motorForce.motorForce2 = -1.5
+         motorForce.motorForce1 = 1.0
+         break;
+      case hoekUAV < 20 :
+         motorForce.motorForce2 = -3.0
+         motorForce.motorForce1 = 2.0
+         break;
+      case hoekUAV < 30 :
+         motorForce.motorForce2 = -4.5
+         motorForce.motorForce1 = 3.5
+         break;
+      case hoekUAV < 40 :
+         motorForce.motorForce2 = -6.0
+         motorForce.motorForce1 = 5.0
+         break;
+      
+      default :
+         motorForce.motorForce1 = 0.0
+         motorForce.motorForce2 = 0.0
+}
+}
