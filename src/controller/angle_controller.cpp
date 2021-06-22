974230949969895a62258d074577129e6f6d7c1b
JasperVVL <66367196+JasperVVL@users.noreply.github.com>
2021-06-22 14:35:30 +0200

    motor force data in controller bijgevoegd

--- src/controller/angle_controller.cpp
+++ src/controller/angle_controller.cpp
@@ -13,12 +13,12 @@
 
 #include "../../defines.h"
 
-/*
+
 const float m = 1.434;          // Massa in kg
 float F_gyro = 0.01;            // F =  0.01 zodat de kracht niet bij 0 N begint
 float Fmax = 0.11 * 2;          // Maximale stuwkracht, maal 2 want 2 motoren
 float Fmin = -0.12 * 2;         // Minimale stuwkracht, maal 2 want 2 motoren
-const float sp = 1/2*PI;             // setpoint
+const float sp = 0;             // setpoint
 const float Kp_gyro = 2.1;      // Proportionele versterkingsfactor
 const float Kd_gyro = 0.7;      // Versterkingsfactor voor de demping
 const float Ki_gyro = 1.0;      // Ki
@@ -27,12 +27,11 @@ const float Icg = 0.028961;     //Massatraagheidsmoment
 float Mz = 0;                   //Verstoring
 float b = 0.168;                //afstand tussen stuwkrachtmotoren
 const bool simulator = true;
-float F_motor = 0.0;
 long t_oud, t_nw;
 float dt;
 
 float avg_alpha = 0.0;
-float avg_omega = 0.0;*/
+float avg_omega = 0.0;
 
 /**
  * @brief
@@ -42,7 +41,7 @@ float avg_omega = 0.0;*/
  * @param TimeOfFlightData Struct with ToF distance data
  * @param degreesOfFreedomData Struct with 9-axes DoF data
  */
- /*
+ 
 void angle_controller(MotorSettings& motorData, MotorForce& motorForce,
                       TimeOfFlightData& TimeOfFlightData,
                       DoFData& degreesOfFreedomData) {
@@ -51,26 +50,23 @@ void angle_controller(MotorSettings& motorData, MotorForce& motorForce,
   if (t_nw - t_oud > cyclustijd) {
   dt = (t_nw - t_oud) * .001;
   t_oud = t_nw;
-
-  regelaar_verstoring();
-  gyro_data();
-
-  alpha = (F_gyro * b + Mz)/ Icg;      
-  omega = omega + alpha * dt; 
-  theta = theta + omega * dt; 
-
+  
 
   error_gyro = sp - theta;
   d_error_gyro = error_gyro - error_gyro_oud;
   error_gyro_som = error_gyro_som + error_gyro*dt;
-  float F_gyro = Kp_gyro * error_gyro + Kd_gyro * d_error_gyro/dt
-  + Ki_gyro * error_gyro_som *dt;
-  F_gyro = constrain((F_gyro/2), (Fmin/2), (Fmax/2));
+  float F_gyro = Kp_gyro * error_gyro + Kd_gyro * d_error_gyro/dt + Ki_gyro * error_gyro_som *dt;
+   
+  constrain(F_gyro, Fmin, Fmax)); 
   error_gyro_oud = error_gyro;
-  Kracht_naar_PWM(constrain(F_motor - F_gyro, Fmin, Fmax),
-  constrain(F_motor + F_gyro, Fmin, Fmax)); 
+
+   
+  float motorForceData = F/2;
+   
+  motorForce.motor1Force = motorForceData;
+  motorForce.motor2Force = motorForceData;
   }
 }
 
-}
-*/
+
+
